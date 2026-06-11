e657a38ceae3b64c4fcf6d4c481f0b271733b5db
aShinde <ananta.shinde@honeywellcom>
2026-06-11 11:00:27 +0530

    11/06/2026

--- oops/demo02.cpp
+++ oops/demo02.cpp
@@ -11,13 +11,17 @@ class Subject{
 
     int id;
     string name;
-   
+    
 
     public:
      Syllabus syllabus;
      Subject(int id,string name){
          this->id = id;
          this->name = name;
+    }
+
+    string getName(){
+        return name;
     }
 
 };
@@ -29,7 +33,7 @@ class Course{
     public:
     string name;
     vector<Subject*> subjects;
-
+    int count = 0;
     Course(int id,string name){
          this->id = id;
          this->name = name;
@@ -43,7 +47,7 @@ class Student{
     int rollNo;
     string name;
     public:
-    Course* mycourse;
+    Course** mycourse;
 
     Student(int rollNo,string name){
         this->rollNo = rollNo;
@@ -67,43 +71,67 @@ class Student{
 
 class CourseManager{
    
-    vector<Course> courselist; 
+    vector<Course*> courselist; 
 
     public:
-    void CreateNewCourse(){
+    void createNewCourse(){
         string name;
         cout<<"enter course name:";
-        cin>>name;
+        cin >> name;
         Course* course = new Course(courselist.size()+1,name);
-        cout<< "to save course you must add atleast one subject to it"<<endl;
-        cout<<"enter subject name:";
-        cin>> name;
-        Subject* subject =new Subject(course->subjects.size()+1,name);
-        cout<< "to save subject you must add atleast one topic to it"<<endl;
-        cout<<"enter topic name:";
-        cin>> name;
-        subject->syllabus.topics.push_back(name);
-        course->subjects.push_back(subject);
+        cout<< "to course  it must have atleast subject:"<<endl;
+        char subchoice;
+        do{
+            cout<<"enter subject name:";
+            cin >> name;
+            Subject* subject = new Subject(course->count+1,name);
+            cout<< "to create subject it must have atleast one topic :"<<endl;
+            char choice ;
+            do {
+                cout<<"enter topic name:";
+                cin >> name;
+                subject->syllabus.topics.push_back(name);
+            
+
+                cout<<"want to add more tiopics(Y/N):";
+                cin >> choice;
+            }while(choice =='Y');
+
+            course->subjects.push_back(subject);
+
+            cout<<"want to add more subjects(Y/N):";
+            cin >> subchoice;
+        } while(subchoice == 'Y'); 
+
+        courselist.push_back(course);
+
     }
 
     void printCourseDetails(int id){
-        for(auto itr=courselist.begin();itr != courselist.end();itr++)
-        {
-            if(itr->getId() == id){
-                cout<< "name of course :"<<itr->name;
-                // for(Subject* sitr=itr->subjects.begin();sitr != itr->subjects.end();sitr++)
-                // {
-                //     cout<<sitr->syllabus
-                // }
+        
+      for(int i=0;i<courselist.size();i++){
+        if(courselist[i]->getId() == id){
+            Course* course = courselist[i];
+            cout << "course details:"<<endl;
+            cout << course->name;
+            cout <<" subjects :"<<endl;
+            course->subjects;
+            for(int j=0;j<course->subjects.size();j++){
+                  cout << course->subjects[j]->getName();
             }
+
         }
+        
+      }
     }
 };
 
 int main(int argc, char const *argv[])
 {
    // create new course
-  
+   CourseManager cm;
+//    cm.createNewCourse();
+   cm.printCourseDetails(1);
 
     return 0;
 }
